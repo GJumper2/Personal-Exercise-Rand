1c41954ac3e1a520f2b2191d4377b8f1e412fbde
Gerrit Grove <Gerrit Grove@EZAUDBIBOMAZERO>
2025-08-07 12:47:34 +0200

    Add project files.

---
+++ Exercise/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercise
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            Output.Content = userInput.Text;
+            if (userInput.Text == Output.Content.ToString())
+            {
+                outputStatus.Content = "Msg send successful";
+                userInput.Text = "";
+            }
+
+            else
+            {
+                outputStatus.Content = "msg not sent...";
+            }
+
+
+        }
+
+    }
+}
